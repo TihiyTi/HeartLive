9d8f5b6ffec7eedfbde6fe7293422d3d79364696
tihiyti <tihiy-ti@mail.ru>
2012-09-21 13:23:08 +0400

    Prepare to built agent ver 2

--- test/communication/SignalReaderTest.java
+++ test/communication/SignalReaderTest.java
@@ -1,21 +1,21 @@
-package communication;
-
-import junit.framework.TestCase;
-import service.Settings;
-import communication.SignalReader;
-
-import java.io.File;
-
-/**
- * User: Tihiy
- * Date: 17.09.12
- * Time: 15:14
- */
-public class SignalReaderTest extends TestCase {
-    public void testStartRead() throws Exception {
-        Settings config = new Settings("COM3");
-        SignalReader sr = new SignalReader(new File("C:\\Users\\aleksey\\IdeaProjects\\HeartLive\\test\\sirvice\\samples.txt"));
-       // sr.startRead();
-    }
-
-}
+//package communication;
+//
+//import junit.framework.TestCase;
+//import service.Settings;
+//import communication.SignalReader;
+//
+//import java.io.File;
+//
+///**
+// * User: Tihiy
+// * Date: 17.09.12
+// * Time: 15:14
+// */
+//public class SignalReaderTest extends TestCase {
+//    public void testStartRead() throws Exception {
+//        Settings config = new Settings("COM3");
+//        SignalReader sr = new SignalReader(new File("C:\\Users\\aleksey\\IdeaProjects\\HeartLive\\test\\sirvice\\samples.txt"));
+//       // sr.startRead();
+//    }
+//
+//}
