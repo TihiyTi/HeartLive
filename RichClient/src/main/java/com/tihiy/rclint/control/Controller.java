aafdfc6f28941e310151cd21088c26904ab036fc
tihiyti <tihiy-ti@mail.ru>
2012-10-02 16:54:41 +0400

    Begin to add MVC  - develop MVC arhitipe

---
+++ RichClient/src/main/java/com/tihiy/rclint/control/Controller.java
@@ -0,0 +1,20 @@
+package com.tihiy.rclint.control;
+
+import com.tihiy.rclint.mvcAbstract.AbstractController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: aleksey
+ * Date: 02.10.12
+ * Time: 16:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class Controller extends AbstractController {
+
+//    public static final String
+
+    @Override
+    protected void setCommand(String command) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
