8f5b48d44bcb312a4a1602f67ebd682aa0a49ae8
tihiyti <tihiy-ti@mail.ru>
2012-10-03 15:02:16 +0400

    Begin to add MVC  - develop MVC arhitipe

---
+++ RichClient/src/main/java/com/tihiy/rclint/view/SignalPanel.java
@@ -0,0 +1,14 @@
+package com.tihiy.rclint.view;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: aleksey
+ * Date: 03.10.12
+ * Time: 14:59
+ * To change this template use File | Settings | File Templates.
+ */
+public class SignalPanel extends JPanel {
+
+}
