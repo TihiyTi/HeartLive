adc4b87571db6cdb8a9755133b0ec446a19daf59
AlexTi <tihiy-ti@mail.ru>
2013-12-25 17:44:35 +0400

    Update SignalPanelLite, SignalModelLite Create DynamicModel

--- RichClient/src/main/java/com/tihiy/rclint/mvcAbstract/DynamicModelInterface.java
+++ RichClient/src/main/java/com/tihiy/rclint/mvcAbstract/DynamicModelInterface.java
@@ -3,5 +3,5 @@ package com.tihiy.rclint.mvcAbstract;
 import java.util.concurrent.BlockingQueue;
 
 public interface DynamicModelInterface<T extends Number> {
-    public void setQueue(BlockingQueue<T> queue);
+    public void setBufferQueue(BlockingQueue<T> queue);
 }
