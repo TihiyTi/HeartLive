1f0449a5198c6dfb0fd819792dcb5be42c61a4ef
AlexTi <tihiy-ti@mail.ru>
2014-09-08 13:48:39 +0400

    Продолжение. Правки направленные на модификацию: парсера - чтение заголовков для возможности составления карты JFreePanel

--- CommunicationFile/src/main/java/com/tihiy/comm/parse/Reo32Parser.java
+++ CommunicationFile/src/main/java/com/tihiy/comm/parse/Reo32Parser.java
@@ -6,6 +6,7 @@ import java.util.*;
 
 public class Reo32Parser {
     Map<Signal, List<Double>> map;
+    Map<String, List<Double>> map2;
 
     public Reo32Parser() {
         map = new HashMap<>();
@@ -54,39 +55,22 @@ public class Reo32Parser {
         return map;
     }
 
-    public Map<Signal, List<Double>> parse(Scanner scanner) {
-        //        CharMatcher trimmer = CharMatcher.JAVA_LETTER;
-        List<String> list = new ArrayList<>();
+    public Map<String, List<Double>> parse(Scanner scanner) {
+        List<String> strings = new ArrayList<>();
         while(scanner.hasNextLine()){
-            list.add(scanner.nextLine());
+            strings.add(scanner.nextLine());
         }
-        Splitter splitter = Splitter.on("\n").trimResults().omitEmptyStrings();
-        splitter = Splitter.on("\t").trimResults().omitEmptyStrings();
-        List<String> s = splitter.splitToList(list.get(0));
-//        System.out.println(list.get(0));
-//        System.out.println(s.toString());
-//        System.out.println(list.get(1));
-        for(int i = 2 ; i < list.size(); i++){
-            List<String> st = splitter.splitToList(list.get(i));
+        Splitter splitter = Splitter.on("\t").trimResults().omitEmptyStrings();
+        List<String> names = splitter.splitToList(strings.get(0));
+        names.forEach(e -> map2.put(e, new ArrayList<>()));
 
-            map.get(Signal.Pulse_1).add(Double.valueOf(st.get(0)));
-            map.get(Signal.Pulse_2).add(Double.valueOf(st.get(2)));
-            map.get(Signal.Pulse_3).add(Double.valueOf(st.get(4)));
-            map.get(Signal.Pulse_4).add(Double.valueOf(st.get(6)));
-            map.get(Signal.Pulse_5).add(Double.valueOf(st.get(8)));
-            map.get(Signal.Pulse_6).add(Double.valueOf(st.get(10)));
-//            map.get(Signal.Base_1).add(Double.valueOf(st.get(1)));
-//            map.get(Signal.Base_2).add(Double.valueOf(st.get(3)));
-//            map.get(Signal.Base_3).add(Double.valueOf(st.get(5)));
-//            map.get(Signal.Base_4).add(Double.valueOf(st.get(7)));
-//            map.get(Signal.Base_5).add(Double.valueOf(st.get(9)));
-//            map.get(Signal.Base_6).add(Double.valueOf(st.get(11)));
-
-            map.get(Signal.ECG).add(Double.valueOf(st.get(st.size()-1)));
+        for(int i = 2 ; i < strings.size(); i++){
+            List<String> st = splitter.splitToList(strings.get(i));
+            for (int j = 0; j < names.size(); j++) {
+                map2.get(names.get(j)).add(Double.valueOf(st.get(j)));
+            }
         }
-        return map;
-
-
+        return map2;
     }
 
     public enum Signal {
