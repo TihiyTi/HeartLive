9a67fbe1436bb51471862ef7d58917971c72d0a7
AlexTi <tihiy-ti@mail.ru>
2015-01-14 13:51:37 +0400

    MinSKOFinder with vis complete

--- implements/param-finder/src/main/java/MinSKOFinder.java
+++ implements/param-finder/src/main/java/MinSKOFinder.java
@@ -1,6 +1,10 @@
+import com.tihiy.WindowUtils;
+import com.tihiy.jfreeclient.SignalJFreePanel;
 import com.tihiy.reonew.SphereCalc;
 import com.tihiy.reonew.SphereModelParam;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,6 +23,7 @@ public class MinSKOFinder {
     private int minH = 0;
     private double minRo = 0;
     private int minY = 0;
+    private List<Double> minModelList;
 
     public static boolean DEBUG = false;
 
@@ -41,6 +46,7 @@ public class MinSKOFinder {
                             minR =r;
                             minRo = ro;
                             minY = y;
+                            minModelList = modelList;
                         }
                     }
                 }
@@ -98,15 +104,46 @@ public class MinSKOFinder {
     public static void main(String[] args) {
         MinSKOFinder finder = new MinSKOFinder();
 //        DEBUG = true;
-        finder.bR = 20; finder.eR = 60;
-        finder.bH = 5; finder.eH = 40;
-        finder.bY = 0; finder.eY = 40;
-        finder.bRo = 4; finder.eRo = 10;
+        finder.bR = 20; finder.eR = 80;
+        finder.bH = 0; finder.eH = 40;
+        finder.bY = 0; finder.eY = 60;
+        finder.bRo = 4; finder.eRo = 15;
 //        List<Double> expList = Arrays.asList(38.335, 38.492, 38.716, 38.999);
+//        List<Double> expList = Arrays.asList(32.4, 33.5, 34.7);
         List<Double> expList = Arrays.asList(28.,30.,31.3,33.,34.6,35.3,35.,34.6,34.9,33.7,36.,36.6);
+//        List<Double> expList = Arrays.asList(32.2, 30.8, 31.4, 31.2, 32.4, 31.8, 33.7, 34.6, 35.5, 34.8,
+//                35.3, 34.7, 35., 35.5,34.3);
+//        List<Double> expList = Arrays.asList(32.4, 30.7, 32.5, 31.6, 31.9, 33.4, 35.4, 36.9, 37.6,
+//                38.6, 41., 43.2);
+//        List<Double> expList = Arrays.asList(37., 39.5, 41.3, 43.,45.);
+
         finder.getParamWithMinSKO(expList);
         finder.printResult();
-
+        finder.getSignalInFrame("Signals", finder.minModelList, expList);
 //        finder.getModelData(5, 20, 42, 5, 35);
+    }
+
+    public void getSignalInFrame(String frameName, List<Double> modelList, List<Double> expList){
+        JFrame frame = new JFrame("Signal");
+        frame.setTitle(frameName);
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        frame.setSize(new Dimension(800, 500));
+        frame.getContentPane().add(getSignal(modelList, expList));
+        WindowUtils.centerOnScreenAndSetVisible(frame);
+    }
+    public JPanel getSignal(List<Double> modelList, List<Double> expList){
+        return getPanelWithSignal(modelList, expList);
+    }
+
+    private JPanel getPanelWithSignal(List<Double> oneList, List<Double> twoList) {
+        List<List<Double>> listOfSignal = new ArrayList<>();
+        List<String> listOfNames = new ArrayList<>();
+        listOfSignal.add(oneList);
+        listOfSignal.add(twoList);
+        listOfNames.add("ModelList");
+        listOfNames.add("ExpList");
+        List<Double> listOfScales = Arrays.asList(0.05,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.);
+        SignalJFreePanel signalJFreePanel = new SignalJFreePanel(listOfSignal, listOfNames);
+        return signalJFreePanel;
     }
 }
